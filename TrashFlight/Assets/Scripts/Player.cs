9e454e772026053b5700d3228344e5d814f83676
penny <pennyyi97@gmail.com>
2025-01-17 21:18:50 +0900

    캐릭터 마우스로 좌우 움직일 수 있게 & Clamp로 영역 밖으로 벗어났을 때 캐릭터 위치 처리

--- TrashFlight/Assets/Scripts/Player.cs
+++ TrashFlight/Assets/Scripts/Player.cs
@@ -12,11 +12,19 @@ public class Player : MonoBehaviour
         // Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f); //어디로 이동할지 정해주는 값
         // transform.position += moveTo * moveSpeed * Time.deltaTime;
 
+        //키보드로 제어
+        /*
         Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
         if (Input.GetKey(KeyCode.LeftArrow)){ //왼쪽 방향키 눌렀을 때 왼쪽으로 움직이도록 음수값으로 지정
             transform.position -= moveTo;
         } else if (Input.GetKey(KeyCode.RightArrow)){ //오른쪽 방향키 눌렀을 때 오른쪽으로 움직이도록 양수값으로 지정
             transform.position += moveTo;
         }
+        */
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //게임 화면이랑 유니티 내부의 position이랑 단위 일치시키기
+        float toX = Mathf.Clamp( mousePos.x, -2.35f, 2.35f); //value가 최솟값보다 작으면 최솟값을, 크면 최대값으로 설정하게 해주는 method
+
+        transform.position = new Vector3(toX, transform.position.y, transform.position.z); //마우스 위치로 플레이어 x 위치 변경, y&z는 현재 위치 고정
     }
 }
