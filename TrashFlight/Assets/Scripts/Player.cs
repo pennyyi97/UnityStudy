e2e715186c448465be350524877698450157d124
penny <pennyyi97@gmail.com>
2025-01-17 21:43:18 +0900

    미사일 만들기, prefabs로 미사일 여러개 발사되도록 함 & 자동으로 캐릭터 머리에서 발사되도록 위치 설정 (instantiate)

--- TrashFlight/Assets/Scripts/Player.cs
+++ TrashFlight/Assets/Scripts/Player.cs
@@ -1,9 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] //유니티에서 직접 값을 변경후 확인할 수 있도록 함
     private float  moveSpeed;
+    [SerializeField]
+    private GameObject weapon;
+
+    [SerializeField]
+    private Transform shootTransform;
+
     void Update()
     {
         // float horizontalInput = Input.GetAxisRaw("Horizontal"); //키보드 방향키 왼/오
@@ -26,5 +33,11 @@ public class Player : MonoBehaviour
         float toX = Mathf.Clamp( mousePos.x, -2.35f, 2.35f); //value가 최솟값보다 작으면 최솟값을, 크면 최대값으로 설정하게 해주는 method
 
         transform.position = new Vector3(toX, transform.position.y, transform.position.z); //마우스 위치로 플레이어 x 위치 변경, y&z는 현재 위치 고정
+
+        Shoot();
+    }
+
+    void Shoot() {
+        Instantiate(weapon,shootTransform.position, Quaternion.identity);
     }
 }
---
+++ TrashFlight/Assets/Scripts/Weapon.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10;
+
+    void Update()
+    {
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+    }
+}
