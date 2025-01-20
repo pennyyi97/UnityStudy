83d106ad0ef7bef7dfc2dbf99b4c2e0c22144603
penny <pennyyi97@gmail.com>
2025-01-20 17:43:01 +0900

    적 무한 생성하기

--- TrashFlight/Assets/Scripts/EnemySpawner.cs
+++ TrashFlight/Assets/Scripts/EnemySpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -6,15 +7,30 @@ public class EnemySpawner : MonoBehaviour
     private GameObject[] enemies;
     private float[] arrPosX = {-2.2f, -1.1f, 0f, 1.1f, 2.2f};
 
+    [SerializeField]
+    private float spawnInterval = 1.5f;
     void Start()
     {
-        foreach (float pos in arrPosX)
-        {
-            int index = Random.Range(0, enemies.Length); //랜덤으로 enemy의 인덱스 뽑아주기
-            SpawnEnemy(pos, index);
-            
-        }
+        StartEnemyRoutine();
+    }
 
+    void StartEnemyRoutine(){
+        //다음작업이 기다리는 동안에도 계속 게임이 진행될 수 있도록 병렬로 작업되는 루틴인 것 같다..
+        StartCoroutine("EnemyRoutine");
+    }
+
+    IEnumerator EnemyRoutine(){
+        yield return new WaitForSeconds(3f); //괄호 안의 시간동안 다음작업하기까지 기다림
+        while (true){
+            foreach (float pos in arrPosX)
+            {
+                int index = Random.Range(0, enemies.Length); //랜덤으로 enemy의 인덱스 뽑아주기
+                SpawnEnemy(pos, index);
+                
+            }
+
+            yield return new WaitForSeconds(spawnInterval); //foreach가 끝나고 잠깐 기다렸다가 다시 시작. setTimeout같은건가?
+        }
     }
 
     void SpawnEnemy(float posX, int index) {
