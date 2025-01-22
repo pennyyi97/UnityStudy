a9b2af193cb8cb63b6e6e5e0cb7046795f723364
penny <pennyyi97@gmail.com>
2025-01-22 23:21:21 +0900

    게임오버 패널 적용

--- TrashFlight/Assets/Scripts/GameManager.cs
+++ TrashFlight/Assets/Scripts/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour
     private TextMeshProUGUI text;
     private int coin = 0;
 
+    [SerializeField]
+    private GameObject gameOverPanel;
+
     [HideInInspector] //public 이지만 유니티에서는 숨김처리
     public bool isGameOver = false; //게임 오버 판단
 
@@ -40,10 +43,18 @@ public class GameManager : MonoBehaviour
     //게임 오버 확인
     public void SetGameOver(){
         isGameOver = true;
-        
+
         EnemySpawner enemySpawner = FindAnyObjectByType<EnemySpawner> ();
         if (enemySpawner != null){
             enemySpawner.StopEnemyRoutine();
         }
+
+        //invoke를 쓰면 두번째 인자(시간)만큼 지난 후 첫번째 인자(메소드) 실행 => setTimeout같은거인 것 같다.
+        Invoke("ShowGameOverPanel", 1f);
+    }
+
+    void ShowGameOverPanel(){
+        //게임오버되면 게임오버 패널 보이게 설정
+        gameOverPanel.SetActive(true);
     }
 }
